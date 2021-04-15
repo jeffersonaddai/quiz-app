4457ec5b7d4b066fdac8c934961192a08fe55b25
jefferson <jeffepok@gmail.com>
2021-04-15 13:02:23 +0000

    Added a reset function in highscores.html

--- assets/scripts/highscores.js
+++ assets/scripts/highscores.js
@@ -1,19 +1,27 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 let highScoresCategory = document.getElementById('highScoresCategory');
-
+const reset = document.getElementById("reset");
 showHighScores();
 
 highScoresCategory.addEventListener('change', showHighScores);
+reset.addEventListener('click', resetFn)
 
-
+function resetFn(){
+  localStorage.clear();
+  showHighScores();
+}
 function showHighScores(){
-  highScoresList.innerHTML = highScores
-  .map(score => {
-    if(score.category === highScoresCategory.value){
-      return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    }
-  })
-  .join("");
+  if(highScores.length > 0){
+    highScoresList.innerHTML = highScores
+    .map(score => {
+      if(score.category === highScoresCategory.value){
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+      }
+    })
+    .join("");
+  }else{
+    highScoresList.innerHTML = '<li class="high-score">No scores Available</li>';
+  }
 }
 
